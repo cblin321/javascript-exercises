de39d361be8ed891010a1db8fa9b5346bfc854ec
Chris Lin <chrislin90210@gmail.com>
2023-08-27 13:48:46 -0500

    finished all exercises

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    n = parseFloat(n);
+    if (n < 0)
+        return "OOPS";
+    if (n == 1)
+        return 1;
+    if (n == 2)
+        return 1;
+    return fibonacci(n - 1) + fibonacci(n - 2);
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((acc, curr) => {
+        const death1 = !("yearOfDeath" in acc) ? (new Date()).getFullYear() : acc.yearOfDeath;
+        const death2 = !("yearOfDeath" in curr) ? (new Date()).getFullYear() : curr.yearOfDeath;
+        const lifespan1 = (death1 - acc.yearOfBirth);
+        const lifespan2 = (death2 - curr.yearOfBirth);
+        return lifespan1 < lifespan2 ? curr : acc; 
+    });
 };
 
 // Do not edit below this line
